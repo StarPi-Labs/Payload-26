a94340bb85f690a1df68d1c0eb974c2737d6c1f8
Samuele-Bradke <samu.bradke@gmail.com>
2026-03-28 11:14:11 +0100

    Bluetooth Fix

--- Embedded-Code/main/bt_serial_bridge.c
+++ Embedded-Code/main/bt_serial_bridge.c
@@ -8,6 +8,7 @@
 #include "esp_bt.h"
 #include "esp_bt_main.h"
 #include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
 #include "esp_spp_api.h"
 
 static const char *TAG = "BT_BRIDGE";
@@ -26,6 +27,13 @@ static esp_bt_mode_t bt_controller_mode_for_build(void)
 #else
     return ESP_BT_MODE_CLASSIC_BT;
 #endif
+}
+
+static void bt_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
+{
+    if (event == ESP_BT_GAP_MODE_CHG_EVT) {
+        ESP_LOGI(TAG, "GAP mode changed: mode=%d", param->mode_chg.mode);
+    }
 }
 
 static void bt_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
@@ -104,6 +112,18 @@ bool bt_serial_init(const char *device_name)
     ret = esp_bluedroid_enable();
     if (ret != ESP_OK && ret != ESP_ERR_INVALID_STATE) {
         ESP_LOGE(TAG, "bluedroid enable failed: %s", esp_err_to_name(ret));
+        return false;
+    }
+
+    ret = esp_bt_gap_register_callback(bt_gap_cb);
+    if (ret != ESP_OK && ret != ESP_ERR_INVALID_STATE) {
+        ESP_LOGE(TAG, "gap callback register failed: %s", esp_err_to_name(ret));
+        return false;
+    }
+
+    ret = esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+    if (ret != ESP_OK && ret != ESP_ERR_INVALID_STATE) {
+        ESP_LOGE(TAG, "gap set scan mode failed: %s", esp_err_to_name(ret));
         return false;
     }
 
