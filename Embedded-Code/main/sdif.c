6cff331355346e81fe437be2ae4e56f49444b60a
ckevar <ckevar@gmail.com>
2026-04-17 10:15:21 +0200

    SD storage integration

---
+++ Embedded-Code/main/sdif.c
@@ -0,0 +1,52 @@
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+#include "sdif.h"
+
+#define SD_MOUNTING_POINT   "/sd"
+
+#ifdef CONFIG_ENABLE_SD_SPI
+
+esp_err_t sys_hardware_sd_init(spi_host_device_t spi_port) {
+    esp_err_t ret;
+    spi_bus_config_t bus_cfg = {
+        .mosi_io_num = GPIO_NUM_13,
+        .miso_io_num = GPIO_NUM_12,
+        .sclk_io_num = GPIO_NUM_14, // 14
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 4096
+    };
+    ret = spi_bus_initialize(spi_port,&bus_cfg, SPI_DMA_CH_AUTO);
+
+    return ret;
+}
+
+void sys_fs_unmount(sdmmc_card_t *card) {
+    esp_vfs_fat_sdcard_unmount(SD_MOUNTING_POINT, card);
+}
+
+esp_err_t sys_fs_mount(spi_host_device_t spi_port, sdmmc_card_t *card) {
+    esp_vfs_fat_sdmmc_mount_config_t mount_cfg = {
+        .format_if_mount_failed = false,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024,
+    };
+
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+    host.slot = spi_port;
+
+    sdspi_device_config_t slot_cfg = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_cfg.gpio_cs = GPIO_NUM_15;
+    slot_cfg.host_id = host.slot;
+
+    return esp_vfs_fat_sdspi_mount(SD_MOUNTING_POINT, &host, &slot_cfg, &mount_cfg, &card);
+}
+
+
+#elif SDIO
+esp_err_t SD_HW_init(void) {
+    return ESP_OK;
+}
+
+
+#endif
---
+++ Embedded-Code/main/sdif.h
@@ -0,0 +1,15 @@
+#ifndef _SD_INTERFACE_H_
+#define _SD_INTERFACE_H_
+
+#ifdef CONFIG_ENABLE_SD_SPI
+#include "driver/spi_master.h"
+
+esp_err_t sys_hardware_sd_init(spi_host_device_t spi_port);
+esp_err_t sys_fs_mount(spi_host_device_t spi_port, sdmmc_card_t *card);
+void sys_fs_unmount(sdmmc_card_t *card);
+
+#elif CONFIG_ENABLE_SD_SDIO 
+
+#endif
+
+#endif
